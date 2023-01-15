c895fecf2ccc53319c06e52b3fd53f5c8c64ac0c
Bartek Krzys <bartek@mergedup.com>
2023-01-15 12:26:45 +0100

    🌱 Update seedwork files to allow additional components to be imported directly from seedwork.domain module.

--- sparkly/app/seedwork/domain/__init__.py
+++ sparkly/app/seedwork/domain/__init__.py
@@ -1,6 +1,10 @@
 from .aggergates import Aggregate
 from .commands import Command
 from .entities import Entity
+from .events import DomainEvent
+from .exceptions import DomainValidationError
+from .factories import DomainFactory
 from .queries import Query
 from .repositories import Repository, T_entity
 from .value_objects import ValueObject
+
