b2066cf68cb2ae1a691a2b14f6fba9674d9c3318
Bartek Krzys <bartek@mergedup.com>
2023-01-15 21:13:16 +0100

    👔 Add .add_log function to VehicleLogsRepository.

--- sparkly/app/adapters/repos/postgresql/repositories.py
+++ sparkly/app/adapters/repos/postgresql/repositories.py
@@ -1,6 +1,21 @@
-from sparkly.app.domain import entities
+from pydantic import UUID4
+from sqlalchemy import insert
+from sparkly.app.domain import entities, value_objects
 from sparkly.app.seedwork import adapters
+from sparkly.app.adapters.db.postgres import models
 
 
 class VehicleLogsRepository(adapters.SQLAlchemyRepository[entities.Vehicle]):
-    pass
+    async def add_log(self, vehicle_id: UUID4, log: value_objects.VehicleLog) -> None:
+        stmnt = insert(models.vehicle_logs).values(
+            vehicle_id=str(vehicle_id),
+            timestamp=log.timestamp.value,
+            speed=log.speed.value,
+            speed_unit=log.speed.unit,
+            odometer=log.odometer.value,
+            odometer_unit=log.odometer.unit,
+            state_of_charge=log.state_of_charge.value,
+            elevation=log.elevation.value,
+            elevation_unit=log.elevation.unit,
+        )
+        await self.session.execute(stmnt)
