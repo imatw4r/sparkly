82c6764ee167bd8bfe1cac6d49c70bc9fdfddc1c
Bartek Krzys <bartek@mergedup.com>
2023-01-19 00:20:27 +0100

    ✅ Write working test that doesn't leave any resources.

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -3,6 +3,16 @@ from fastapi import FastAPI
 from httpx import AsyncClient
 
 from sparkly.app.entrypoints.http.fastapi.main import app as api
+from sparkly.app.seedwork import domain, service_layer
+
+
+class TestCommandBus(service_layer.CommandBus):
+    def __init__(self) -> None:
+        self.commands: list[domain.Command] = []
+
+    async def handle(self, message: domain.Command) -> service_layer.HandlerResult:
+        self.commands.append(message)
+        return service_layer.HandlerResult(result=None)
 
 
 @pytest.fixture(scope="session")
@@ -12,4 +22,9 @@ def app() -> FastAPI:
 
 @pytest.fixture(scope="session")
 def test_client(app: FastAPI) -> AsyncClient:
-    return AsyncClient(app=app)
+    return AsyncClient(app=app, base_url="http://sparkly")
+
+
+@pytest.fixture()
+def command_bus() -> service_layer.CommandBus:
+    return TestCommandBus()
--- tests/integration/endpoints/test_vehicle.py
+++ tests/integration/endpoints/test_vehicle.py
@@ -1,14 +1,23 @@
 import pytest
 from fastapi import FastAPI, status
 from httpx import AsyncClient
+from pydantic import UUID4
+
+from sparkly.app.domain import commands
+from tests.integration.conftest import TestCommandBus
 
 pytestmark = pytest.mark.asyncio
 
 
 class TestCreateVehicle:
-    async def test_create_vehicle(self, app: FastAPI, test_client: AsyncClient) -> None:
-        res = await test_client.post(
-            app.url_path_for("vehicles:create-vehicle"), json={"vehicle_id": "03103e56-ba9b-4c79-830a-4018e2f4962c"}
-        )
+    async def test_create_vehicle(self, app: FastAPI, test_client: AsyncClient, command_bus: TestCommandBus) -> None:
+        with app.container.message_busses.command.override(command_bus):  # type: ignore[attr-defined]
+            res = await test_client.post(
+                app.url_path_for("vehicles:create-vehicle"),
+                json={"vehicle_id": "03103e56-ba9b-4c79-830a-4018e2f4962c"},
+            )
 
-        assert res.status_code == status.HTTP_201_CREATED
+            assert res.status_code == status.HTTP_201_CREATED
+            assert command_bus.commands[0] == commands.AddVehicle(
+                vehicle_id=UUID4("03103e56-ba9b-4c79-830a-4018e2f4962c")
+            )
